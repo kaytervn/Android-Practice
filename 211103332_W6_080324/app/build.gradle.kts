2184eb2df006039e1962b164b6355e6d23c4f5af
KayterCantFixBug <118961250+KayterCantFixBug@users.noreply.github.com>
2024-03-12 08:03:55 +0700

    a

--- 211103332_W6_080324/app/build.gradle.kts
+++ 211103332_W6_080324/app/build.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "vn.kayterandroid.a21110332_bt4_010324"
+    namespace = "vn.kayterandroid.a211103332_w6_080324"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "vn.kayterandroid.a21110332_bt4_010324"
+        applicationId = "vn.kayterandroid.a211103332_w6_080324"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
@@ -30,14 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
-    implementation ("com.intuit.sdp:sdp-android:1.1.0")
-    implementation("androidx.core:core-ktx:1.12.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
