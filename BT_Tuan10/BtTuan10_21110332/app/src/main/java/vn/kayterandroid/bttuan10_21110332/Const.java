fd08a41df4befc298db1096c1903fcdc0640c95a
KayterCantFixBug <kienductrong@gmail.com>
2024-04-19 07:56:03 +0700

    update btqt

--- BT_Tuan10/BtTuan10_21110332/app/src/main/java/vn/kayterandroid/bttuan10_21110332/Const.java
+++
@@ -1,6 +0,0 @@
-package vn.kayterandroid.bttuan10_21110332;
-
-public class Const {
-    public static final String MY_USERNAME = "username";
-    public static final String MY_IMAGES = "avatar";
-}
