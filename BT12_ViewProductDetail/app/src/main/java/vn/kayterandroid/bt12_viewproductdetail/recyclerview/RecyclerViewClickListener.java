ab529513d034c71abfd3cbd5af842f00d282a200
KayterCantFixBug <kienductrong@gmail.com>
2024-04-23 10:43:58 +0700

    a

--- BT12_ViewProductDetail/app/src/main/java/vn/kayterandroid/bt12_viewproductdetail/recyclerview/RecyclerViewClickListener.java
+++
@@ -1,7 +0,0 @@
-package vn.kayterandroid.bt12_viewproductdetail.recyclerview;
-
-import vn.kayterandroid.bt12_viewproductdetail.model.Food;
-
-public interface RecyclerViewClickListener {
-    void onItemClick(Food food);
-}
